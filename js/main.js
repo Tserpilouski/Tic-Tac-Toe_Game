a65923638c4eeb4d84c08845ee26fbc5762e39d0
Kiryl Tserpilouski <terpilovskikirill@gmail.com>
2023-02-27 23:07:00 +0100

    Added function to add characters to the field

--- js/main.js
+++ js/main.js
@@ -12,27 +12,103 @@
 // }
 
 //dodaanie hover efect 
+
+let first_move = 2;
+let path, idName, pathConst;
+
+if(first_move === 1){
+    first_move = 2;
+    path = "/icons/icon-o-outline.svg";
+    pathConst = "/icons/icon-o.svg"
+    idName = "prevO"
+}
+else{
+    first_move = 1;
+    path = "/icons/icon-x-outline.svg";
+    pathConst = "/icons/icon-x.svg"
+    idName = "prevX"
+}
+
 const itemBoard = document.querySelectorAll('.board__item');
 const imgPreview = document.createElement('img');
-const imgConst = document.createElement('img');
-imgPreview.src = "/icons/icon-o-outline.svg";
-imgConst.src = "/icons/icon-o.svg";
+imgPreview.src = path;
+imgPreview.id = idName;
+const div_array = Array.prototype.slice.call(itemBoard);
 
 
+div_array.forEach((element) => {
 
-itemBoard.forEach((element) => {
-    console.log(element)
     element.addEventListener('mouseenter', () => {
-        console.log(`mouse in`);
-        element.appendChild(imgPreview);
+        if(!element.hasChildNodes()){
+            element.appendChild(imgPreview);
+        }
     });
 
-    element.addEventListener('mouseleave', () => {
-        console.log(`mouse out`);
-        element.removeChild(imgPreview);
+    element.addEventListener('mouseleave',() => {
+        if(element.childNodes[0].id == idName){
+            element.removeChild(imgPreview);
+        }
     });
 
     element.addEventListener("click", () => {
-        element.appendChild(imgConst);
+        if(element.childNodes[0].id == idName){
+            element.removeChild(imgPreview);
+            console.log('del')
+        }
+        if(!element.hasChildNodes()){
+            let imgConst = document.createElement('img');
+            imgConst.src = pathConst;
+            imgConst.id = "2"
+            element.appendChild(imgConst);
+            console.log("add element")            
+        };
     });
-})
+
+    element.onclick = function bro() { 
+        console.log(first_move)
+        first_move === 2 ? first_move = 1 : first_move = 2;
+        console.log(first_move)
+    }
+});
+bro();
+//__Sprawdzenie znaczkow__
+
+// const data = [ [1, 0, 2],
+//                [0, 2, 0],
+//                [2, 1, 1]];
+
+// let firstDig = 1;
+// let secondDig = 1;
+
+// let win;
+
+// for(let i = 0; i < 3; i++){
+//     firstDig = firstDig * data[i][i];
+//     secondDig = secondDig * data[2 - i][i];
+//     if ((firstDig || secondDig) === 1){
+//         win = "win X";
+//         console.log(`Dioganal ${win}`);
+//         break;
+//     }
+//     else if ((firstDig || secondDig) === 8){
+//         win = "win O";
+//         console.log(`Dioganal ${win}`);
+//         break;
+//     }
+//     let row = 1;
+//     let col = 1;
+//     for(let j = 0; j < 3; j++){
+//         row = row * data[i][j];
+//         col = col * data[j][i];
+//     }
+//     if ((row || col) === 8){
+//         win = "win O";
+//         break;
+//     }
+//     else if ((row || col) === 1){
+//         win = "win X";
+//         break;
+//     }
+// }
+
+// console.log(win);
